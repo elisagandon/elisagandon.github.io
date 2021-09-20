a1c81ceadbf0b2c83e48672b288c140b2428d081
elisagandon <72449355+elisagandon@users.noreply.github.com>
2021-09-20 13:02:19 +0200

    php.article

---
+++ php/articles.php
@@ -0,0 +1,71 @@
+<!-- 
+  &#160; = espace insécable (empèche le passage à la ligne)
+
+  &#8201; = espace fine (avec la ponctuation double (:, !, ?,""))
+
+  tous les codes -> https://www.la-grange.net/w3c/html4.01/sgml/entities.html#h-24.4
+
+-->
+
+
+<section>
+      
+  <aside>    
+    <h2>
+      Elisa Gandon
+      <a href="https://elisagandon.github.io/" target="_blank">portfolio</a>
+    </h2>
+    <p>
+      Design participatif 
+    </p>
+  </aside>
+
+
+<!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="filterDiv tag1 tag2">
+    <h3>
+      Pierre MUSSO, Laurent PONTHOU, Éric SEULLIET.
+ (auteur).  
+    </h3>
+
+    <h4>
+      <a href="#mettre le lien" target="_blank">
+        Design du codesign
+        Le rôle de la communication dans le design participatif 
+      </a>
+    </h4> 
+    
+    <h5>
+      Pierre MUSSO, Laurent PONTHOU, Éric SEULLIET, 2007.
+    </h5>
+    
+    <blockquote cit="#">Le principe fondamental de la démocratie est que les fins de liberté et d’individualité pour tous ne peuvent être atteintes que par des moyens en accord avec ces fins.
+John Dewey, 1937</blockquote>
+    
+    <p> 
+      Cet article est destiné à présenter le design coopératif que l’on appelle aussi design participatif ou codesign. Il discute de la question de la création participative en régime démocratique et adopte une approche relationniste et communicationnelle typique du codesign et du design empathique.
+    </p>
+   
+
+    <h6> 
+      20 Septembre   
+    </h6>
+  
+  </article>
+
+
+
+
+
+</section>
+
+
+
+
