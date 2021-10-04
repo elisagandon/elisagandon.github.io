786d06d2fb3ad518e833fd4c1493fb5d05891ce8
elisagandon <72449355+elisagandon@users.noreply.github.com>
2021-10-04 13:22:47 +0200

    article_php_4oct

---
+++ INR_elisa/articles.php
@@ -0,0 +1,254 @@
+<!-- 
+  &#160; = espace insécable (empèche le passage à la ligne)
+
+  &#8201; = espace fine (avec la ponctuation double (:, !, ?,""))
+
+  tous les codes -> https://www.la-grange.net/w3c/html4.01/sgml/entities.html#h-24.4
+
+-->
+
+
+<section>
+      
+  <aside>    
+    <h2>
+      Elisa Gandon
+      <a href="https://elisagandon.github.io/" target="_blank">portfolio</a>
+    </h2>
+    <p>
+      La conception participative, ou coconception, que l'on retrouve couramment sous le terme codesign, co-design et design participatif, est une méthode de travail qui implique l’utilisateur final, lors d’un processus d’échange et de création, dans la conception d’un produit ou d’un service. 
+    </p>
+  </aside>
+
+
+<!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="filterDiv co-design design">
+    <h3>
+      Pierre MUSSO, Laurent PONTHOU, Éric SEULLIET.
+ (auteur).  
+    </h3>
+
+    <h4>
+      <a href="https://www.mei-info.com/wp-content/uploads/2017/06/MEI40-143-160-Design-du-codesign.pdf" target="_blank">
+        Design du codesign
+        Le rôle de la communication dans le design participatif 
+      </a>
+    </h4> 
+    
+    <h5>
+      Pierre MUSSO, Laurent PONTHOU, Éric SEULLIET, 2007.
+    </h5>
+    
+    <blockquote cit="#">Le principe fondamental de la démocratie est que les fins de liberté et d’individualité pour tous ne peuvent être atteintes que par des moyens en accord avec ces fins.<br/>
+John Dewey, 1937</blockquote>
+    
+    <p> 
+      Cet article est destiné à présenter le design coopératif que l’on appelle aussi design participatif ou codesign. Il discute de la question de la création participative en régime démocratique et adopte une approche relationniste et communicationnelle typique du codesign et du design empathique.
+    </p>
+   
+
+    <h6> 
+      Consulté le 20 Septembre 2021  
+    </h6>
+  
+  </article>
+
+  <!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="filterDiv co-design design">
+    <h3>
+      Cité en chantier, Collectif Ne Rougissez Pas  
+    </h3>
+
+    <h4>
+      <a href="http://www.nerougissezpas.fr/projets/chantiers/cite-en-chantier/">
+        CITÉ EN CHANTIER 
+        Viry Chatillon
+        Juin 2016
+        Experience - Chantier - Collectif 
+      </a>
+    </h4> 
+    
+    <h5>
+      Collectif Ne Rougissez Pas, 2016.
+    </h5>
+    
+    <blockquote cit="#">À partir d'une balade urbaine réalisée par le CAUE 91 et de l'engagement de 15 jeunes en réinsertion, le projet de signalement et de signalétique de la cité des Erables a émergé.</blockquote>
+    
+    <p> 
+      Cinq jours de chantier, de rencontres, de repas, de dessins, de découpes, de pauses, de peintures, de pochoirs et d'installations à Viry Châtillon.
+Signalétique créée en panneaux de bois découpés et peints.
+    </p>
+   
+
+    <h6> 
+      Consulté le 27 Septembre 2021
+    </h6>
+  
+  </article>
+
+ <!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="filterDiv co-design design">
+    <h3>
+      Captain LUDD 
+    </h3>
+
+    <h4>
+      <a href="http://cptludd.fr/index.php?article74/rosa">
+        LA ROSA : une typographie créee par les enfants de l'école Rosa Parks
+      </a>
+    </h4> 
+    
+    <h5>
+      Captain LUDD, 2019.
+    </h5>
+    
+    <blockquote cit="#">Une typographie unique, à partir d'une grille inspirée des vitraux , la Rosa sert de texte aux panneaux. Avec cette grille les enfants ont crée ensemble un alphabet commun.</blockquote>
+    
+    <p> 
+      Au cours de 3 séances d'ateliers dans l'école Rosa parks de Saint-Etienne, le collectif Captain LUDD a crée avec les enfants une typographie et une collection de pictogramme afin de réaliser la signalétique de leur école.
+Des formes ont été tirées des nombreux vitraux de l'école, ils nous ont permis de réalisé la grille typographique utilisé comme un Geo-plan afin que les enfants composent des caractère sur la même base.
+
+    </p>
+   
+
+    <h6> 
+      Consulté le 4 Octobre 2021
+    </h6>
+  
+  </article>
+
+<!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="filterDiv co-design design">
+    <h3>
+      Éloïsa Pérez  
+    </h3>
+
+    <h4>
+      <a href="https://www.eloisaperez.fr/">
+        Du geste à l'idée : formes de l'écriture à l'école primaire 
+      </a>
+    </h4> 
+    
+    <h5>
+      Éloïsa Pérez, 2014.
+    </h5>
+    
+    <blockquote cit="#">L’écriture est avant tout un système d’expression. Son acquisition implique l’apprentissage des logiques graphiques, syntaxiques et sémantiques qui régissent le langage qu’elle figure.</blockquote>
+    
+    <p> 
+      Les recherches d’Eloïsa Pérez interrogent l’apprentissage de la lecture et de l’écriture par l’enfant. Comment le design graphique peut-il accompagner les objectifs pédagogiques de l’enfant ? Comment travailler et moduler cet apprentissage à l’aune des influences de systèmes visuels dans le processus d’acquisition de ce savoir ?
+    </p>
+   
+
+    <h6> 
+      Consulté le 4 Octobre 2021  
+    </h6>
+  
+  </article>
+
+  <!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="filterDiv co-design design">
+    <h3>
+      Grapheine  
+    </h3>
+
+    <h4>
+      <a href="https://www.grapheine.com/branding/autre-soie-identite-visuelle-participative">
+        L’Autre Soie, une identité visuelle participative ! 
+      </a>
+    </h4> 
+    
+    <h5>
+      Grapheine, 06 janvier 2019
+    </h5>
+    
+    <blockquote cit="#">Et peu importe que les points soient reliés physiquement ou mentalement, c'est dans cette action que les mots deviennent des signes, et que le logo apparait. Dès lors, la personne qui regarde le logo devient co-auteur du logo !</blockquote>
+    
+    <p> 
+       Dans cet article le studio Grapheine présente un projet d'identité visuelle qui s'aventure hors des sentiers battus. Une aventure de conception collective qui fut l'occasion d'interroger le rôle de l'identité visuelle en dehors des champs institutionnels et commerciaux classiques. Ainsi l'enjeu fut d'essayer de répondre à la question : À quoi ressemblerait une identité visuelle horizontale, participative et inclusive ?
+    </p>
+   
+
+    <h6> 
+      Consulté le 4 Octobre 2021  
+    </h6>
+  
+  </article>
+
+<!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="filterDiv tag1 tag2">
+    <h3>
+      Team Thursday   
+    </h3>
+
+    <h4>
+      <a href="https://etapes.com/team-thursday-moving-futures/">
+      Moving Futures 
+      </a>
+    </h4> 
+    
+    <h5>
+      Team Thursday, 2014
+    </h5>
+    
+    <blockquote cit="#">Personnaliser 2000 affiches à la main, c’est du boulot, mais ça peut valoir le coup de tenter l’effort.</blockquote>
+    
+    <p> 
+      L’idée à travers les supports de communication a été de réaliser une affiche typographique modèle, sur laquelle figure toutes les informations comme le nom des villes, les dates ou encore les thèmes du festival. Ensuite, armée d’un marqueur rouge, l’équipe de Team Thursday, s’est attaquée à chaque affiche, pour les personnaliser spontanément et mettre en évidence les informations importantes en fonction des représentations et déplacements du festival.
+    </p>
+   
+
+    <h6> 
+      Consulté le 4 Octobre 2021  
+    </h6>
+  
+  </article>
+
+
+</section>
+
+
+
+
